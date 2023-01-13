d8cb4e7ff8bd7dcb5b3e219257307f08e89cd3f9
Chris Ess <caess@ithiriel.com>
2023-01-13 00:09:19 -0500

    Add mix_test_interactive.

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule CocktailIndex.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:wallaby, "~> 0.30.0", runtime: false, only: :test},
-      {:ex_machina, "~> 2.7", only: :test}
+      {:ex_machina, "~> 2.7", only: :test},
+      {:mix_test_interactive, "~> 1.0", runtime: false, only: :dev}
     ]
   end
 
