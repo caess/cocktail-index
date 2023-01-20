a9044553c1a6ab36ff739fcd8ff9faed3aa29a8f
Chris Ess <caess@ithiriel.com>
2023-01-19 23:11:52 -0500

    Add test.

--- test/cocktail_index_web/controllers/cocktail_controller_test.exs
+++ test/cocktail_index_web/controllers/cocktail_controller_test.exs
@@ -14,4 +14,19 @@ defmodule CocktailIndexWeb.CocktailControllerTest do
       assert response =~ "has already been taken"
     end
   end
+
+  describe "update/2" do
+    test "renders edit page with errors when data is invalid", %{conn: conn} do
+      insert(:cocktail, name: "elixir")
+      cocktail = insert(:cocktail)
+      params = string_params_for(:cocktail, name: "elixir")
+
+      response =
+        conn
+        |> put(Routes.cocktail_path(conn, :update, cocktail), %{"cocktail" => params})
+        |> html_response(200)
+
+      assert response =~ "has already been taken"
+    end
+  end
 end
