34d2f969a119e71993327840fc63811e17d217c1
Chris Ess <caess@ithiriel.com>
2023-01-22 17:31:34 -0500

    Increase size of test pool.

--- config/test.exs
+++ config/test.exs
@@ -11,7 +11,7 @@ config :cocktail_index, CocktailIndex.Repo,
   hostname: "127.0.0.1",
   database: "cocktail_index_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 10
+  pool_size: 20
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
